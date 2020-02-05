d267d5189b8fb046d26f24a6793830df1c2d94b6
Bilha <32512727+bilha-analytics@users.noreply.github.com>
2020-02-05 18:06:18 +0300

    nltk @ corpus resources and bigram formation

--- DocReader/docReader/PlayTextBlob.py
+++ DocReader/docReader/PlayTextBlob.py
@@ -208,4 +208,132 @@ def nltk_corpora():
     # << txt files. Use PlaintextCorpusReader. Check dir location 
     # 
     my_corpus = PlaintextCorpusReader( "root_dir_path_here", ".*") # second param is a list of fileids defined as a list or an ls pattern
+    eg_corpus = PlaintextCorpusReader( "D:/zRepoz/dataSaysWhat/DocReader/res/txt_corpus", ".txt")
+    eg_corpus.fileids()
+    eg_corpus.words( "example1.txt")
+    len( eg_corpus.sents() )
+
+    #BracketParseCorpusReader
+    my_corpus = nltk.corpus.BracketParseCorpusReader( "path", "file_pattern")
     
+
+def ConditionalFrequencyDistributions():
+    # recall: FreqDist( a_list_of_words ) 
+    # e.g. fd = nltk.Freqdist( eg_corpust.words("example1.txt") )
+    #       fd.plot( )
+    #       fd.tabulate( )
+    # Now: nltk.ConditionalFreqDist() does grouped by freqDist. I.E. 
+    #       It is a data type; a collection of freqdists, each one for a different condition, where condition is the groupby category
+    # Params: (condition, event) << a list of pairs
+    #       condition: the groupby category e.g. language, genre
+    #       event: what is to be counted e.g word length, 
+    #       [ (x, y) for x in X for y in Y ]
+    genre_word = [ (genre, word) for genre in ["news", "romance"] for word in brown.words(categories=genre ) ]
+    genre_word = [ (genre, len(word) ) for genre in ["news", "romance"] for word in brown.words(categories=genre ) ]
+
+    cfd = ConditionalFreqDist( genre_word )
+    cfd.conditions() # list of genres 
+    cfd
+    print( cfd["news"] )
+    cfd["news"].most_common( 10 ) 
+    cfd["news"]["could"]
+    cfd["news"][3] 
+    cfd.plot( cumulative=True )
+
+    # another: freq of citizen in each year/doc i.e. ( "citizen", 1895) = pair of keywords for each year/doc
+    cfd = nltk.ConditionalFreqDist( (cat, fileid[:4] ) 
+        for fileid in inaugural.fileids()
+        for w in inaugural.words( fileid)
+        for cat in ["america", "citizen", "freedom", "slave"]
+        if w.lower().startswith( cat ) 
+        )
+    cfd.plot( cumulative=True)
+    cfd.plot( cumulative=True, conditions=["america", "citizen"])
+
+    # Exercise: CFD @ news and romance genres of brown corpus, find which days of the week are most newsworthy
+    gw = [ (genre, dow ) for genre in ["news", "romance"] for dow in brown.words( categories=genre) if dow.lower() in dayz]
+    cdf = nltk.ConditionalFreqDist( gw ) 
+    cdf.plot() 
+
+
+def generatingRandomTextWithBigrams():
+    # can use conditional freq to create a table of bigrams(word pairs). 
+    # fun bigram() takes a list of words and builts a list of consecutive word pairs. i.e. list( nlkt.bigrams(sentense) ) 
+    #
+    txt3_bigrams = nltk.bigrams( book.text3 ) 
+    cfd = nltk.ConditionalFreqDist( txt3_bigrams ) 
+    def genRandomText( cfd_src, seed_txt, n=15):
+        s = ""
+        for i in range(n):
+            s = s+seed_txt+", "
+            seed_txt = cfd_src[ seed_txt].max() 
+        return s
+    print( genRandomText( cfd, "living" ) ) 
+
+def moreCorpusStuff_LexicalResources():
+    # local english vocab file = nltk.corpus.words 
+    def unsualwords( corp_txt ):
+        en_ok = set( w.lower() for w in nltk.corpus.words.words() ) 
+        que = set( w.lower() for w in corp_txt if w.isalpha() ) 
+        return( sorted( que - en_ok ) ) 
+    
+    # stop words = nltk.corpus.stopwords
+    nltk.corpus.stopwords.words( "english" ) 
+    def proportionNonStopWords( corp_txt ):
+        stopz = nltk.corpus.stopwords.words( "english") 
+        content = [ w for w in corp_txt if w.lower() not in stopz ] 
+        return len( content ) / len( corp_txt )
+
+    # wordlist corpus used in a game 
+    # target word game = generate list of words with all the letters in the puzzle used at least once and the middle puzzle letter is a must
+    def targetWordGame( ):
+        puzzle_letters_fq = nltk.FreqDist( "egivrvonl")
+        oblig = "r"
+        wordslist = nltk.corpus.words.words()
+        return [ w for w in wordslist if len(w) > 5 and oblig in w and nltk.FreqDist(w) <= puzzle_letters_fq ]
+
+    # names corpus : 8K++ first names categorized by gender 
+    names = nltk.corpus.names
+    names.fileids() # different files for different gender 
+    male_names = names.words( "male.txt") 
+    female_names = names.words( "female.txt") 
+    unisex_names = [w for w in male_names if w in female_names ]
+
+    # last letter of male Vs female names
+    cfd = nltk.ConditionalFreqDist( (gender, name_tail[-1] ) for gender in nltk.corpus.names.fileids()  for name_tail in nltk.corpus.names.words(gender) )
+    cfd.plot() 
+
+    # Pronouncing Dictionary 
+    # english CMU pronouncing dict = nltk.corpus.cmudict << frp, Arpabet. <<< 1 - primary stress, 2 = secondary stress, 0 = no stress
+    len( nltk.corpus.cmudict.entries() )
+    entries = nltk.corpus.cmudict.entries() 
+    len( entries )
+    for e in entries[23432:23439]: # view subset - a list of phonetic codes for each word 
+        print( e )
+    
+    def findRhymingWords( word_phonemes=['N', 'IH0', 'K', 'S'] ): #NICKS 
+        return[ w for w, p in entries if p[-4:] == word_phonemes]
+    findRhymingWords( nltk.corpus.cmudict.dict()["fits"][0] )
+    
+    # list of words where mn is pronounced M
+    [ w for w, p in entries if w[-1] == 'n' and p[-1] == 'M']
+    # list of words where start is not n but pronounced N
+    [ w for w, p in entries if w[0] != 'n' and p[0] == 'N']
+    # set of phonemes not starting with n byt pronounced as N
+    set( [w[:2] for w, p in entries if w[0] != 'n' and p[0] == 'N'] )
+
+    # find words with similar intonation <<< 1 - primary stress, 2 = secondary stress, 0 = no stress
+    def stress(pron):
+        return [ char for phone in pron for char in phone if char.isdigit() ]
+    [ w for w, pron in entries if stress( pron) == ['0', '1', '0', '2', '0']] 
+    [ w for w, pron in entries if stress( pron) == ['0', '2', '0', '1', '0']] 
+    # find words with minimally contrasting sets of words using FregDist
+    def closeWords(phone='P', word_len=3):
+        p3 = [ ( pron[0]+"-"+pron[2],w) for w, pron in entries if pron[0] == phone and len(pron) == word_len] 
+        cfd = nltk.ConditionalFreqDist( p3 ) 
+        for temp in sorted( cfd.conditions() ): 
+            if len( cfd[temp] ) > 10:
+                words = sorted( cfd[ temp ] )
+                wordstring = ", ".join( words)
+                print( temp, wordstring[:70], "...")
+
